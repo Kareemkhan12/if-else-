c6a1f74eb9993d00ac4c19cf63f37c15dd4ba6c7
Kareemkhan12 <124231580+Kareemkhan12@users.noreply.github.com>
2023-02-08 22:08:30 +0500

    Add files via upload

---
+++ subtraction, multiple, division and sum.cpp
@@ -0,0 +1,24 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+   int a,b,multiple, division,sum, subtraction;
+cout<<"a=";
+cin>>a;
+cout<<"b=";
+cin>>b;
+multiple =a*b;//two value are multiple
+cout<<"multiple=";
+cout <<multiple;
+division =a/b;//two value are division 
+cout <<"division=";
+cout <<division;
+sum=a+b;//two values are sum
+cout <<"sum=";
+cout <<sum;
+subtraction =a-b; 
+cout <<"subtraction=";
+cout <<subtraction;
+return 0;
+}
