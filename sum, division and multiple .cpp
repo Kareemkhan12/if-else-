d6bdabf35616b1cd8349ea6bd2b4f68c15177ed4
Kareemkhan12 <124231580+Kareemkhan12@users.noreply.github.com>
2023-02-08 22:13:52 +0500

    Add files via upload

---
+++ sum, division and multiple .cpp
@@ -0,0 +1,21 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+   int a,b,multiple, division,sum;
+cout<<"a=";
+cin>>a;
+cout<<"b=";
+cin>>b;
+multiple =a*b;
+cout<<"multiple=";
+cout <<multiple;
+division =a/b;
+cout <<"division=";
+cout <<division;
+sum=a+b;
+cout <<"sum=";
+cout <<sum;
+return 0;
+}
