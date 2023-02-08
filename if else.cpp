e0eabe7391542994310bd1bc8911514821869f5d
Kareemkhan12 <124231580+Kareemkhan12@users.noreply.github.com>
2023-02-08 22:12:46 +0500

    Add files via upload

---
+++ if else.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    int time =20;
+    if(time>18)
+    {
+    cout<<"good morning";
+    cin>>time;
+    }
+    else 
+    {
+    cout<<"good night";
+    cin >>time;
+    }
+    return 0;
+    }
+    
